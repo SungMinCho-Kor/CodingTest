c272d5ec1f720e1bbe3db2b76dde652b726a7768
SungMinCho-Kor <128480641+SungMinCho-Kor@users.noreply.github.com>
2023-05-12 21:51:29 +0900

    [level 2] Title: 소수 찾기, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,27 +1,28 @@
-import itertools
 
 def sosoo(n):
     cnt = 0
-    print(int(n**0.5))
+    if n<2:
+        return False
+    elif n==2:
+        return True
     for i in range(2, int(n**0.5) + 1):
         if n%i == 0:
             return False
     return True
     
+def permutation(base, array):
+    if base:
+        s.add(int(base))
+    for i, j in enumerate(array):
+        permutation(base + j, array[:i] + array[i+1:])
     
+s = set()
 def solution(numbers):
     answer = 0
-    ls = list(numbers)
-    s = set()
-    for i in range(1,len(numbers) + 1):
-        tmp = list(itertools.permutations(ls,i))
-        for j in tmp:
-            j = int("".join(j))
-            if j ==1 or j == 0:
-                continue
-            s.add(j)
+    permutation("",list(numbers))
     print(s)
     for i in s:
         if sosoo(i):
+            print(i)
             answer+=1
     return answer
