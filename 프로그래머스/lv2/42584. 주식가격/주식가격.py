771f681fb336146becb344c0268fea2150b72e73
SungMinCho-Kor <128480641+SungMinCho-Kor@users.noreply.github.com>
2023-05-21 23:58:59 +0900

    [level 2] Title: 주식가격, Time: 106.50 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,29 +1,29 @@
-from collections import deque
-
-def solution(prices):
-    answer =[]
-    for i in range(len(prices)):
-        j = i
-        while j <len(prices):
-            if prices[i]<=prices[j]:
-                j+=1
-            else:
-                j+=1
-                break
-        answer.append(j-i-1)
-    return answer
-
+# from collections import deque
 
 # def solution(prices):
-#     stack = []
-#     answer = [0] * len(prices)
+#     answer =[]
 #     for i in range(len(prices)):
-#         while stack != [] and stack[-1][1] > prices[i]:
-#             past, _ = stack.pop()
-#             answer[past] = i - past
-#             # print(answer)
-#         stack.append([i, prices[i]])
-#         print(stack)
-#     for i, s in stack:
-#         answer[i] = len(prices) - 1 - i
+#         j = i
+#         while j <len(prices):
+#             if prices[i]<=prices[j]:
+#                 j+=1
+#             else:
+#                 j+=1
+#                 break
+#         answer.append(j-i-1)
 #     return answer
+
+
+def solution(prices):
+    stack = []
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        while stack != [] and stack[-1][1] > prices[i]:
+            past, _ = stack.pop()
+            answer[past] = i - past
+            # print(answer)
+        stack.append([i, prices[i]])
+        print(stack)
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
+    return answer
