950ee71638eb2d821e0ca1045d492ae4bfd84bab
SungMinCho-Kor <128480641+SungMinCho-Kor@users.noreply.github.com>
2023-06-02 02:28:23 +0900

    [level 2] Title: 전화번호 목록, Time: 95.92 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,6 @@
 def solution(phone_book):
     phone_book.sort()
     for i in range(len(phone_book) - 1):
-        for j in range(i+1,len(phone_book)):
-            if phone_book[j][:len(phone_book[i])] == phone_book[i]:
-                return False
-            else:
-                break
-    
-
+        if phone_book[i+1][:len(phone_book[i])] == phone_book[i]:
+            return False
     return True
