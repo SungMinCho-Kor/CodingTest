04f66ff4ee8559774039df0c7bac84dc4fb56ed8
SungMinCho-Kor <128480641+SungMinCho-Kor@users.noreply.github.com>
2024-07-10 18:52:12 +0900

    [Bronze III] Title: ZOAC 4, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -1,9 +1,12 @@
 import sys
 
+def function(H, W, N, M):
+    
+    width = W//(M+1) + (1 if W%(M+1) > 0 else 0)
+    height = H//(N+1) + (1 if H%(N+1) > 0 else 0)
+    
+    return width * height
+    
 H, W, N, M = map(int, sys.stdin.readline().rstrip().split())
 
-width = W//(M+1) + (1 if W%(M+1) > 0 else 0)
-height = H//(N+1) + (1 if H%(N+1) > 0 else 0)
-answer = width * height
-
-print(answer)
+print(function(H, W, N, M))
