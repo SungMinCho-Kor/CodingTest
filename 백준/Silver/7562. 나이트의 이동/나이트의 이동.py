b5a0bca5ddd8d5c938f63a04ca273cb93f2351cd
SungMinCho-Kor <128480641+SungMinCho-Kor@users.noreply.github.com>
2024-07-30 21:30:29 +0900

    [Silver I] Title: 나이트의 이동, Time: 2832 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -13,31 +13,31 @@ dy = [-1, 1, -2, 2, -2, 2, -1, 1]
 test_case = int(sys.stdin.readline().rstrip())
 for _ in range(test_case):
     l = int(sys.stdin.readline().rstrip())
-    point = tuple(map(int, sys.stdin.readline().rstrip().split()))
-    target_point = tuple(map(int, sys.stdin.readline().rstrip().split()))
+    x, y = map(int, sys.stdin.readline().rstrip().split())
+    target_x, target_y = map(int, sys.stdin.readline().rstrip().split())
     
-    if point == target_point:
+    if x == target_x and y == target_y:
         print(0)
         continue
     
-    board = [[0 for _ in range(l)] for _ in range(l)]
-    tmp_i, tmp_j = point
-    board[tmp_i][tmp_j] = 1
+    board = [[-1 for _ in range(l)] for _ in range(l)]
+    board[x][y] = 0
     
     queue = deque()
-    queue.append(point)
+    queue.append((x, y))
     
-    while queue:
+    break_flag = False
+    while queue and not break_flag:
         i, j = queue.popleft()
         for k in range(8):
             nx = dx[k] + i
             ny = dy[k] + j
             
-            if 0<=nx<l and 0<=ny<l and board[nx][ny] == 0:
+            if 0<=nx<l and 0<=ny<l and board[nx][ny] == -1:
                 board[nx][ny] = board[i][j] + 1
-                if (nx, ny) == target_point:
-                    print(board[nx][ny] - 1)
-                    queue = deque() # while 과 for 둘다 끝내기
+                if nx == target_x and ny == target_y:
+                    print(board[nx][ny])
+                    break_flag = True
                     break
                 else:
                     queue.append((nx, ny))
