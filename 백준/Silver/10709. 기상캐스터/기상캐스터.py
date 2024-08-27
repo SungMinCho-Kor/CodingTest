f61c52156b02839f2ce9663623bbc79524c937c4
SungMinCho-Kor <128480641+SungMinCho-Kor@users.noreply.github.com>
2024-08-27 21:48:25 +0900

    [Silver V] Title: 기상캐스터, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.py
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.py
@@ -7,12 +7,11 @@ sky = [list(input().rstrip()) for _ in range(h)]
 region = [[-1 for _ in range(w)] for _ in range(h)]
 
 for i in range(h):
-    k = 0
-    while k < w:
-        if sky[i][k] == 'c':
-            region[i][k] = 0
-        elif k > 0 and region[i][k-1] >= 0:
-            region[i][k] = region[i][k-1] + 1
-        k += 1
+    for j in range(w):
+        if sky[i][j] == 'c':
+            region[i][j] = 0
+        elif j > 0 and region[i][j-1] >= 0:
+            region[i][j] = region[i][j-1] + 1
+    
 for line in region:
     print(*line)
