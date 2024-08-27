7c6cac5cf0c9b31e92b390ee6e4435c7e90f6988
SungMinCho-Kor <128480641+SungMinCho-Kor@users.noreply.github.com>
2024-08-27 21:51:21 +0900

    [Silver V] Title: 기상캐스터, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.py
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.py
@@ -4,14 +4,12 @@ input = sys.stdin.readline
 h, w = map(int, input().split())
 
 sky = [list(input().rstrip()) for _ in range(h)]
-region = [[-1 for _ in range(w)] for _ in range(h)]
 
 for i in range(h):
+    region = [-1] * w
     for j in range(w):
         if sky[i][j] == 'c':
-            region[i][j] = 0
-        elif j > 0 and region[i][j-1] >= 0:
-            region[i][j] = region[i][j-1] + 1
-    
-for line in region:
-    print(*line)
+            region[j] = 0
+        elif j > 0 and region[j-1] >= 0:
+            region[j] = region[j-1] + 1
+    print(*region)
