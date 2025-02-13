0bdf06d59f5bb3bcb3e41c2b73cc74389a476245
SungMinCho-Kor <128480641+SungMinCho-Kor@users.noreply.github.com>
2025-02-14 00:52:33 +0900

    [Bronze V] Title: A+B - 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 while True:
-    a, b = map(int, input().strip().split())
+    a, b = map(int, input().split())
     if a == 0 and b == 0:
         break
     print(a+b)
